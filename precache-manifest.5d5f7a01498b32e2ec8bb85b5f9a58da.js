6b1ba648432bef882f6b4c606e40010dd0e1f427
Abdul Samad <Samk8778@yahoo.com>
2020-11-10 12:51:08 +0530

    Updates

---
+++ precache-manifest.5d5f7a01498b32e2ec8bb85b5f9a58da.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b23f674d62501bdf6cd35dbe028b400f",
+    "url": "/github_finder/index.html"
+  },
+  {
+    "revision": "a7ec3fb8ea21f7f3dcde",
+    "url": "/github_finder/static/css/2.b837e323.chunk.css"
+  },
+  {
+    "revision": "a6640a888f3b94d376bc",
+    "url": "/github_finder/static/css/main.df902c58.chunk.css"
+  },
+  {
+    "revision": "a7ec3fb8ea21f7f3dcde",
+    "url": "/github_finder/static/js/2.0babff4e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/github_finder/static/js/2.0babff4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6640a888f3b94d376bc",
+    "url": "/github_finder/static/js/main.3c33ced3.chunk.js"
+  },
+  {
+    "revision": "acceda5072f5305960c3",
+    "url": "/github_finder/static/js/runtime-main.78cecee8.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/github_finder/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/github_finder/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/github_finder/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/github_finder/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/github_finder/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
